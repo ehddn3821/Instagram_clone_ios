2d48dfb98385bce959a4377f0392b7b5f3b1e794
dwkang <ehddn3821@naver.com>
2021-06-11 19:12:50 +0900

    #8 탐색화면 컬렉션뷰 및 서치바 추가,  카메라화면 네비게이션바 히든

--- Instagram/Controllers/Core Tabs/CameraViewController.swift
+++ Instagram/Controllers/Core Tabs/CameraViewController.swift
@@ -6,12 +6,17 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    private func didTapTakePicture() {
         
     }
 }
